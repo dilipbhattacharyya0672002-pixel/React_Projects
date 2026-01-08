113e16672621f29159ec71e490feb6f2ba8d1216
Dip <dilipbhattacharyya0672002@gmail.com>
2026-01-08 12:50:29 +0530

    Initital setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import Accordion from './Project_Components/Accordion.jsx'
+import Portfolio from './Project_Components/Portfolio/Portfolio.jsx'
+import Header from './Project_Components/Portfolio/Header.jsx'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
     </>
   )
 }
---
+++ src/Project_Components/Portfolio/Header.jsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+    return (
+        <p>
+            <Link to="/portfolio">Header</Link>
+        </p>
+    )
+}
